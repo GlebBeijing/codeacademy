a7ff7546e8848ed9c844c7971ec34e1ef37ac1e4
GlebBeijing <95107401+GlebBeijing@users.noreply.github.com>
2022-04-21 08:58:34 +0800

    Update code.py

--- code.py
+++ code.py
@@ -9,3 +9,20 @@ print(most_popular1)
 print(most_popular2)
 print(most_popular3)
 print("Can't take this")
+
+
+
+#Functions test
+def trip_planner_welcome(name):
+  print("Welcome to tripplanner v1.0 " + name)
+trip_planner_welcome("Gleb")
+def estimated_time_rounded(estimated_time):
+  rounded_time = round(estimated_time)
+  return rounded_time
+estimate = estimated_time_rounded(4.7)
+def destination_setup(origin, destination, estimated_time, mode_of_transport = "Car"):
+  print('Your trip starts off in ' + origin)
+  print('And you are traveling to ' + destination)
+  print('You will be traveling by ' + mode_of_transport)
+  print('It will take approximately ' + str(estimated_time) + ' hours')
+destination_setup('Summer palace', "Lama Temple", estimate)
